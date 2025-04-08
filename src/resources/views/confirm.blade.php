9cc2903daffea8bff022e5133847bd71ec64a183
ayana <wencaishanben@gmail.com>
2025-04-08 15:34:33 +0900

    20250408 cssファイルの作成　index.css

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -33,11 +33,32 @@
                   <input type="text" name="name" value="サンプルテキスト">
                 </td>
               </tr>
-              
+              <tr class="confirm-table__row">
+                <th class="confirm-table__header">メールアドレス</th>
+                <td class="confirm-table__text">
+                  <input type="email" name="email" value="サンプルテキスト">
+                </td>
+              </tr>
+              <tr class="confirm-table__row">
+                <th class="confirm-table__header">電話番号</th>
+                <td class="confirm-table__text">
+                  <input type="tell" name="tel" value="サンプルテキスト">
+                </td>
+              </tr>
+              <tr class="confirm-table__row">
+                <th class="confirm-table__header">お問い合わせ内容</th>
+                <td class="confirm-table__text">
+                  <input type="text" name="content" value="サンプルテキスト">
+                </td>
+              </tr>
             </table>
+          </div>
+          <div class="form__button">
+            <button class="form__button-submit" type="submit">送信</button>
           </div>
         </form>
     </div>
   </main>
 </body>
+
 </html>
